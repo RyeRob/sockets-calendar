d27ebf103d0b3e61e8a5e829d660a503a83e7682
RyeRob <ryan.robinson5@gmail.com>
2019-06-17 00:36:38 -0400

    added favicon, started styling, started calendar

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+var express = require('express');
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-
+var favicon = require('serve-favicon');
+var path = require('path');
+//serve favicon
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+// serve static files in public directory
+app.use('/public', express.static('public'));
+//serve html
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
-
+//When use connects and dissconects
 io.on('connection', function(socket){
     console.log('a user has connected');
     socket.on('disconnect', function(){
